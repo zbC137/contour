2ee88434a1bd4430187c6e4bd8daf771ff7f9d9d
zbC137 <me-bin.zhang@connect.polyu.hk>
2022-06-01 20:47:48 +0800

    update

---
+++ tem.m
@@ -0,0 +1,23 @@
+a1 = [data.f1.cd(1:2:end,1), data.f1.cd(2:2:end,1)];
+a2 = [data.f1.cds(1:2:end,1), data.f1.cds(2:2:end,1)];
+a3 = [data.f1.cs(1:2:end,1), data.f1.cs(2:2:end,1)];
+a4 = [data.c0(1:2:end), data.c0(2:2:end)];
+
+b1 = [data.f1.cd(1:2:end,701), data.f1.cd(2:2:end,701)];
+b2 = [data.f1.cds(1:2:end,701), data.f1.cds(2:2:end,701)];
+b3 = [data.f1.cs(1:2:end,701), data.f1.cs(2:2:end,701)];
+b4 = [data.f1.c(1:2:end, 701), data.f1.c(2:2:end, 701)];
+
+c1= [data.f1.cd(1:2:end,1401), data.f1.cd(2:2:end,1401)];
+c2 = [data.f1.cds(1:2:end,1401), data.f1.cds(2:2:end,1401)];
+c3 = [data.f1.cs(1:2:end,1401), data.f1.cs(2:2:end,1401)];
+c4 = [data.f1.c(1:2:end, 1401), data.f1.c(2:2:end, 1401)];
+
+d1 = [data.f1.cd(1:2:end,end), data.f1.cd(2:2:end,end)];
+d2 = [data.f1.cds(1:2:end,end), data.f1.cds(2:2:end,end)];
+d3 = [ data.f1.cs(1:2:end,end), data.f1.cs(2:2:end,end)];
+d4 = [data.f1.c(1:2:end, end), data.f1.c(2:2:end, end)];
+
+t = data.t';
+dcoff = abs(data.dcoff');
+e = abs(data.e');
