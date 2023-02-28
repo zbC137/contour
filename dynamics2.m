093df9f7e42b7de199677ce9169459b549fc07e2
zbC137 <me-bin.zhang@connect.polyu.hk>
2023-02-28 20:38:46 +0800

    update

--- dynamics2.m
+++ dynamics2.m
@@ -10,11 +10,11 @@ dX = zeros(3*N, 1);
 for i=1:N
     R = [cos(X(2*N+i)), -0.01*sin(X(2*N+i)); sin(X(2*N+i)), 0.01*cos(X(2*N+i))];
     ui = R\u(2*i-1:2*i);
-    if ui(1)>0.1
-        ui(1) = 0.1;
-    elseif ui(1)<-0.1
-        ui(1)=-0.1;
-    end
+%     if ui(1)>5
+%         ui(1) = 5;
+%     elseif ui(1)<5
+%         ui(1)=-5;
+%     end
     
     dX(2*i-1) = ui(1)*cos(X(2*N+i));
     dX(2*i) = ui(1)*sin(X(2*N+i));
